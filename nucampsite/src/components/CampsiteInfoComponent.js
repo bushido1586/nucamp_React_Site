98278276826914432a73ceccd7d461827e124cd3
Bradley Reed <shenmusha@yahoo.com>
2021-06-19 13:04:43 -0400

    week 2 assignment

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import {CAMPSITES} from '../shared/campsites'
+
 
 class CampsiteInfo extends Component{
 
@@ -16,12 +18,24 @@ class CampsiteInfo extends Component{
             </div>
         );
     }
+    renderComments(comments){
+        if(comments){
+            return(
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map((comments) =>
+                        <div key={comments.id}>{comments.author}<br></br> --{comments.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}</div>)}
+                </div>
+            );
+        }return <div />;
+    }
 
     render(){
           if(this.props.campsite){
               return (
               <div className="row">
                     {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
                 </div>);
           }          
                 return <div />;
