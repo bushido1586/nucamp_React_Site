52a868ba6f83d50a2bf3478fa6b8c133bb76c608
Bradley Reed <shenmusha@yahoo.com>
2021-06-21 17:04:48 -0400

    Presentaitional and Container COmponents

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -30,15 +30,18 @@ class CampsiteInfo extends Component{
         }return <div />;
     }
 
-    render(){
-          if(this.props.campsite){
-              return (
-              <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
-                </div>);
-          }          
-                return <div />;
+    render() {
+        if (this.props.campsite) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                </div>
+            );
+        }
+        return <div />;
     }
 }
 
