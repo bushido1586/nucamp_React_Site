ec9440b992af4583236193cdb4d89fd578618eb0
Bradley Reed <shenmusha@yahoo.com>
2021-06-24 15:00:31 -0400

    adding breadcrumbs

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -11,7 +13,6 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                     <Card>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardBody>
-                            <CardTitle>{campsite.name}</CardTitle>
                             <CardText>{campsite.description}</CardText>
                         </CardBody>
                     </Card>                
@@ -34,6 +35,16 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         if (props.campsite) {
             return (
                 <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <Breadcrumb>
+                                <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                            </Breadcrumb>
+                            <h2>{props.campsite.name}</h2>
+                            <hr />
+                        </div>
+                    </div>
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments comments={props.comments} />
