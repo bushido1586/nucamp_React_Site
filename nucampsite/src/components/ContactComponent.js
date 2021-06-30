ff63059bb6914858b7208cf495ccfbc13d7ce9a6
Bradley Reed <shenmusha@yahoo.com>
2021-06-30 19:08:48 -0400

    removed erronous import statement

--- nucampsite/src/components/ContactComponent.js
+++ nucampsite/src/components/ContactComponent.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row  } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
-import { min } from 'bn.js';
 
 const required = val => val && val.length; 
 const maxLength = len => val => !val || (val.length <= len);
